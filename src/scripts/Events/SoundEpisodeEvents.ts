11c4afac3ecebfae7e538eb3042c59432c6b7822
Serhii Buniak <90277819+Serhii-Buniak@users.noreply.github.com>
2022-01-15 00:43:36 +0300

    fix counter handler

--- src/scripts/Events/SoundEpisodeEvents.ts
+++ src/scripts/Events/SoundEpisodeEvents.ts
@@ -9,7 +9,12 @@ const episodeCounter = new EpisodeCounter();
 document.addEventListener("DOMContentLoaded", controller.restore.bind(controller));
 
 episodeCounter.addEventListener('change', controller.onEpisodeCounterChange.bind(controller))
-episodeCounter.icrementor.addEventListener('click', () => controller.onEpisodeCounterChange.bind(controller)());
-episodeCounter.decrementor.addEventListener('click', () => controller.onEpisodeCounterChange.bind(controller)());
+
+episodeCounter.icrementor.addEventListener('click', () => {
+    setTimeout(controller.onEpisodeCounterChange.bind(controller), 40);
+});
+episodeCounter.decrementor.addEventListener('click', () => {
+    setTimeout(controller.onEpisodeCounterChange.bind(controller), 40);
+});
 form.indexAddEventListener('change', controller.onFormChange.bind(controller));
 form.nameAddEventListener('change', controller.onFormChange.bind(controller))
